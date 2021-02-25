e51afec4b085c1594204e1e8d7c5930bb202271b
crackzj <crackzj@qq.com>
2021-02-25 14:37:29 +0800

    update

---
+++ pages/login/login.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<button type="primary" @click="login">login</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			login(){
+				uni.switchTab({
+					url:"/pages/index/index"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
